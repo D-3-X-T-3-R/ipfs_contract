378259bfdc5a5041013854fdd403c0a6a3da4cf3
D-3-X-T-3-R <sougata.bhattacharjee94@gmail.com>
2021-09-23 17:43:33 +0530

    Added argument parser module

---
+++ ipfs_contract/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+INPUT=$"test-bed/input.txt"
+
+cargo run -- \
+---file-path ${INPUT} 
--- ipfs_contract/src/main.rs
+++ ipfs_contract/src/main.rs
@@ -2,12 +2,22 @@ use ipfs_api::{IpfsApi, IpfsClient};
 use std::fs::File;
 use storage as store;
 
+mod configuration_parameters;
+
 #[actix_rt::main]
 async fn main() {
+    let app_name = "ipfs_contract";
+
+    let config_param = configuration_parameters::get_configuration_parameters(app_name);
+
     let client = IpfsClient::default();
-    let file = File::open("Cargo.toml").expect("could not read source file");
-    let mut cid: String = "".to_string();
+    let file = File::open(config_param.file_path()).expect(&format!(
+        "could not read source file at {}",
+        config_param.file_path()
+    ));
     
+    let mut cid: String = "".to_string();
+
     match client.add(file).await {
         Ok(res) => cid = res.hash.to_string(),
         Err(e) => eprintln!("error adding file: {}", e),
