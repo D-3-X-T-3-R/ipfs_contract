408ab0ef88460284fd33e86cf3395cfd13b8ac20
D-3-X-T-3-R <sougata.bhattacharjee94@gmail.com>
2021-09-23 14:22:43 +0530

    Added IPFS file upload module

--- ipfs_contract/src/main.rs
+++ ipfs_contract/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use ipfs_api::{IpfsApi, IpfsClient};
+use std::fs::File;
+
+#[actix_rt::main]
+async fn main() {
+    let client = IpfsClient::default();
+    let file = File::open("Cargo.toml").expect("could not read source file");
+    let mut cid: String = "".to_string();
+    
+    match client.add(file).await {
+        Ok(res) => cid = res.hash.to_string(),
+        Err(e) => eprintln!("error adding file: {}", e),
+    }
+    println!("{}", cid);
 }
